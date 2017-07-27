bd4795e45dea1c032555ece99a4ab04db97cb007
Doug Thayer <dougathayer@gmail.com>
2017-07-27 10:40:32 -0700

    Bump sample size up in config

--- background_hang_reporter_job/main.py
+++ background_hang_reporter_job/main.py
@@ -431,7 +431,7 @@ def etl_job(sc, sqlContext, config=None):
         'days_to_aggregate': 21,
         'date_clumping': 2,
         'use_s3': True,
-        'sample_size': 0.05,
+        'sample_size': 0.08,
         'symbol_server_url': "https://s3-us-west-2.amazonaws.com/org.mozilla.crash-stats.symbols-public/v1/",
         'hang_profile_filename': 'hang_profile_128_16000',
         'print_debug_info': False,
