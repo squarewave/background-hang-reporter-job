c1a2a1052d215f9971c7bb1e580fd88ab0b395f8
Doug Thayer <dougathayer@gmail.com>
2018-03-19 23:16:06 -0700

    Fix frame string check in devtools tracking

--- background_hang_reporter_job/tracked.py
+++ background_hang_reporter_job/tracked.py
@@ -12,7 +12,7 @@ class DevtoolsHangs(object):
     def matches_hang(hang):
         #pylint: disable=unused-variable
         stack, duration, thread, runnable, process, annotations, build_date, platform = hang
-        return stack is not None and any(frame is not None and "devtools/" in frame
+        return stack is not None and any(isinstance(frame, basestring) and "devtools/" in frame
                                          for lib, frame in stack)
 
 def get_tracked_stats():
